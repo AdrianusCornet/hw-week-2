8908e804dae8ff1a1dbc7f552df1f795f62eaf34
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-08 13:29:39 +0200

    fetch the corect data

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,10 +1,16 @@
 const fetch = require('node-fetch')
 
 function getTatooineResidents() {
-    
+    let tatooineResidents = []
+
+    fetch('https://swapi.co/api/planets/1/')
+        .then(response => response.json())
+        .then(tatooine => console.log(tatooine.residents))
 }
 function promiseMeAString(strign) {
     
 }
 
-module.exports = { getTatooineResidents, promiseMeAString}
+module.exports = {getTatooineResidents, promiseMeAString}
+
+getTatooineResidents()
