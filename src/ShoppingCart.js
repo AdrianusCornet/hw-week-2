69e9a1627d456539feda82a1fb4672c396d73a47
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-08 10:21:18 +0200

    refactor total()

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -18,25 +18,8 @@ module.exports = class ShoppingCart{
         this.items = []
     }
     total() {
-        function priceAdder(runingTotal, nextItem) {
-
-            console.log('rt =', runingTotal)
-            console.log('ni =', nextItem)
-
-            const itemPrise = nextItem.pricePerUnit
-            const itemCount = nextItem.quantity
-
-            console.log('ip =', itemPrise)
-            console.log('ic =', itemCount)
-
-            const additCost = itemCount * itemPrise
-
-            console.log('ac =', additCost)
-
-            return runingTotal + additCost
-        }
-        const total = this.items.reduce(priceAdder, 0)
-        console.log('t =', total)
-        return total
+        return this.items.reduce((runingTotal, nextItem) => {
+            return runingTotal + nextItem.pricePerUnit * nextItem.quantity
+        }, 0)
     }
 }
