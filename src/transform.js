68eb2e5ce30cb128975c1270410866941480dcf7
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-08 12:26:01 +0200

    filter out children and start addign the rest

--- src/transform.js
+++ src/transform.js
@@ -17,11 +17,37 @@ const childFilterData =[
 /* --- main --- */
 
 function groupAdultsByAgeRange(persons) {
-    
+    return persons.reduce(Sorter, {})
+}
+
+function Sorter(list, person) {
+    if (!isAdult(person)) {
+        return list
+    }
+    if (below21(person)) {
+        if (!list['20 and younger']) {
+            list['20 and younger'] = [person]
+        } else {
+            list['20 and younger'].push(person)
+        }
+    }
+    return list
 }
 
 function isAdult(person) {
     return person.age >= 18 ? true : false
+}
+function below21(person) {
+    return person.age < 21 ? true : false
+}
+function below31(person) {
+    return person.age < 31 ? true : false
+}
+function below41(person) {
+    return person.age < 41 ? true : false
+}
+function below51(person) {
+    return person.age < 51 ? true : false
 }
 
 module.exports.groupAdultsByAgeRange = groupAdultsByAgeRange
@@ -34,3 +60,4 @@ function filterOutChildren(persons) {
 }
 //console.log('cf =', filterOutChildren(childFilterData))
 
+console.log('test =', groupAdultsByAgeRange(childFilterData))
