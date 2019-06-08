90596e035837508106637be9373e44e838e622b2
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-08 09:55:48 +0200

    make getItems() and addItem()

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,5 +1,17 @@
 module.exports = class ShoppingCart{
     constructor() {
-        
+        this.items = []
+    }
+    getItems() {
+        return this.items
+    }
+    addItem(itemName, quantity, price) {
+        this.items.push(
+            {
+                name: itemName,
+                quantity: quantity,
+                pricePerUnit: price
+            }
+        )
     }
 }
