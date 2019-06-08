7812206ac7f5a72c22d928825f482e79ff95109b
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-08 13:46:12 +0200

    make getTatooineResidents()

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,16 +1,12 @@
 const fetch = require('node-fetch')
 
 function getTatooineResidents() {
-    let tatooineResidents = []
-
-    fetch('https://swapi.co/api/planets/1/')
+    return fetch('https://swapi.co/api/planets/1/')
         .then(response => response.json())
-        .then(tatooine => console.log(tatooine.residents))
+        .then(tatooine => tatooine.residents)
 }
 function promiseMeAString(strign) {
     
 }
 
 module.exports = {getTatooineResidents, promiseMeAString}
-
-getTatooineResidents()
